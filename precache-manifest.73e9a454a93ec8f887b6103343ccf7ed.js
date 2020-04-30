fbb98f09b34fe40dcc8ee08a44f2a61ecce7157c
k- <mahat.mejor@gmail.com>
2020-04-29 20:00:57 -0500

    Updates

---
+++ precache-manifest.73e9a454a93ec8f887b6103343ccf7ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57579917b74f3bd3da9f2a31f07ad73b",
+    "url": "/Calendar/index.html"
+  },
+  {
+    "revision": "7b7c0dde7ff533e2e507",
+    "url": "/Calendar/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "289cbf93568653e7b173",
+    "url": "/Calendar/static/js/2.31fce1e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calendar/static/js/2.31fce1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7c0dde7ff533e2e507",
+    "url": "/Calendar/static/js/main.a263cf21.chunk.js"
+  },
+  {
+    "revision": "7235fbc554159fe53f33",
+    "url": "/Calendar/static/js/runtime-main.ed99ddb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
